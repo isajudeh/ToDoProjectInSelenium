7206e68bab75d041250d7de3623fe4e7adc8fa73
Isa Judeh <isajudeh@yahoo.com>
2023-05-25 19:25:13 +0300

    update

--- src/test/java/com/qacart/todo/pages/LoginPage.java
+++ src/test/java/com/qacart/todo/pages/LoginPage.java
@@ -6,7 +6,6 @@ import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 import java.io.IOException;
 
@@ -16,12 +15,6 @@ public class LoginPage extends BasePage {
         super(driver);
     }
 
-//    private WebDriver driver;
-//
-//    public LoginPage(WebDriver driver){
-//        this.driver = driver;
-//        PageFactory.initElements(driver, this);
-//    }
 
     @FindBy(css = "[data-testid=\"email\"]")
     private WebElement InputEmail;
